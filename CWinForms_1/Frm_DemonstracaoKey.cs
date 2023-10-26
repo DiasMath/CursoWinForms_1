551d7fbdfdc2246167b5ebaaed69b4729c7386cb
Matheus Dias <92406256+DiasMath@users.noreply.github.com>
2023-10-25 23:07:45 -0300

    Modulo 3

---
+++ CWinForms_1/Frm_DemonstracaoKey.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CWinForms_1
+{
+    public partial class Frm_DemonstracaoKey : Form
+    {
+        public Frm_DemonstracaoKey()
+        {
+            InitializeComponent();
+        }
+
+        private void Txt_Input_KeyDown(object sender, KeyEventArgs e)
+        {
+            Txt_Msg.AppendText("\r\n" + "Pressionei uma tecla: " + e.KeyCode + "\r\n");
+            Txt_Msg.AppendText("\r\n" + "Codigo da tecla: " + ((int)e.KeyCode) + "\r\n");
+            Txt_Msg.AppendText("\r\n" + "Nome da tecla: " + e.KeyData + "\r\n");
+
+            Lbl_Lower.Text = e.KeyCode.ToString().ToLower();
+            Lbl_Upper.Text = e.KeyCode.ToString().ToUpper();
+        }
+
+        private void Btn_Reset_Click(object sender, EventArgs e)
+        {
+            Txt_Msg.Text = "";
+            Txt_Input.Text = "";
+            Lbl_Lower.Text = "";
+            Lbl_Upper.Text = "";
+        }
+    }
+}
